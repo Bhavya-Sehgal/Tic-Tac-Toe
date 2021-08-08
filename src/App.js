e4d6faa7aca882e61406268f972a9133aea4e5e7
bhavya-sehgal <bhavyasehgal05@gmail.com>
2021-08-08 20:30:49 +0530

    msg using condtional rendering

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Board from './components/Board';
 import History from './components/History';
 import { calculateWinner } from './helpers';
+import StatusMessage from './components/StatusMessage';
 import './styles/root.scss';
 
 const App = () => {
@@ -13,10 +14,6 @@ const App = () => {
   const current = history[currentMove];
 
   const winner = calculateWinner(current.board);
-
-  const message = winner
-    ? `Winner is ${winner}`
-    : `Next player is ${current.isXNext ? 'X' : 'O'}`;
 
   const handleSquareClick = position => {
     if (current.board[position] || winner) {
@@ -43,7 +40,7 @@ const App = () => {
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
-      <h2>{message}</h2>
+      <StatusMessage winner={winner} current={current} />
       <Board board={current.board} handleSquareClick={handleSquareClick} />
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
